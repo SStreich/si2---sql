77a9785ba40d597a10939f7e4dd77a57f5cd3786
SStreich <selene.streich@gmail.com>
2019-02-22 05:31:24 +0100

    Implement function giving nth fibonacci number

---
+++ Fibonacci_number.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION nth_fib(n integer)
+RETURNS INTEGER AS $nth_fib$
+  DECLARE
+  x INTEGER := 0;
+  y INTEGER := 1;
+  m INTEGER := 0;
+  BEGIN
+    IF n = 0 THEN RETURN 0;
+    ELSIF n = 1 THEN RETURN 1;
+    ELSE
+     FOR i in 1..n LOOP
+        m := x + y;
+        x := y;
+        y := m;
+      END LOOP;
+    END IF;
+    RETURN m;
+  END;
+  $nth_fib$ LANGUAGE  plpgsql;
+
+DO $fib$
+  BEGIN
+   IF nth_fib(4) >= 0 THEN
+     raise info 'Fibonacci nth number: %', nth_fib(40);
+   end if;
+  END;
+  $fib$ LANGUAGE  plpgsql;
